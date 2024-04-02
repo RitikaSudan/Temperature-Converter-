d3ad5dae4a7267df7cb9365868e9615470b5209e
RitikaSudan <165682978+RitikaSudan@users.noreply.github.com>
2024-04-01 22:24:28 -0400

    Add files via upload

---
+++ Temperature Converter .py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Tempreture Converter, converting from Celsius to Farenheit 
+
+import statistics 
+
+# Asking the value of the tempreture in Ceulsius 
+
+celsius= float(input("Give the tempreture in Celsius: "))
+
+#Convert from celcius to fahrenheit using formula 
+
+fahrenheit= (celsius* 9/5) + 32
+
+#Printing the tempreture in Fahrenheit
+
+print(f"{celsius} degree in celsius corresponds to {fahrenheit} degree Fahrenheit")
+
+
+
+# In[ ]:
+
+
+
+
